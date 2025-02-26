7e80169b83a31eb2819697f6600912daa88c131f
ODarville <odarv88@yahoo.com>
2025-02-25 21:01:08 -0600

    added vite and react

---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <div>
+            <h1>Hwy 6 Plaza</h1>
+        </div>
+    );
+}
+export default Header;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function Main() {
+    return (
+        <div className="bg-indigo-600">
+            <Header />
+        </div>
+    );
+}
+
+export default Main;
